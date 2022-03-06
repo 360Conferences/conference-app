3c523f4c8c8995c740bd7c4ed73efb0a2d478bdf
Ben Chatelain <ben@octop.ad>
2022-03-06 16:59:54 -0700

    👽 Migrate to new Android embedding
    https://github.com/flutter/flutter/wiki/Upgrading-pre-1.12-Android-projects

--- android/app/src/main/java/com/example/threesixty_conferences/MainActivity.java
+++ android/app/src/main/java/com/example/threesixty_conferences/MainActivity.java
@@ -1,13 +1,7 @@
 package com.example.threesixty_conferences;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
-}
+public class MainActivity extends FlutterActivity {}
